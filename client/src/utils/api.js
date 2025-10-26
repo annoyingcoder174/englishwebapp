2a6e1b9e26c8114eab50b4cac6ab0d0bb06177c6
annoyingcoder174 <tranphuctri1742004@gmail.com>
2025-10-27 01:37:41 +0700

    .

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("✅ API Base URL:", import.meta.env.VITE_API_URL);
+
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api",
 });
